13d8512dcc32972176fb51b44ac63227d714df38
Callum Macdonald <git@callum-macdonald.com>
2026-04-29 11:54:42 +0200

    chore: format

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -17,14 +17,23 @@
   {/if}
 </svelte:head>
 
-<div class="bg-yellow-50 border-l-4 border-yellow-400 px-6 py-3 text-sm text-yellow-800">
+<div
+  class="bg-yellow-50 border-l-4 border-yellow-400 px-6 py-3 text-sm text-yellow-800"
+>
   <strong>Note:</strong> This is a fork of
-  <a class="underline" href="https://github.com/therockstorm/privacy-protect">therockstorm/privacy-protect</a>
+  <a class="underline" href="https://github.com/therockstorm/privacy-protect"
+    >therockstorm/privacy-protect</a
+  >
   created to address
-  <a class="underline" href="https://github.com/therockstorm/privacy-protect/issues/69">#69</a>
+  <a
+    class="underline"
+    href="https://github.com/therockstorm/privacy-protect/issues/69">#69</a
+  >
   and
-  <a class="underline" href="https://github.com/therockstorm/privacy-protect/issues/130">#130</a>.
-  Hopefully those will be fixed upstream and this fork can be retired.
+  <a
+    class="underline"
+    href="https://github.com/therockstorm/privacy-protect/issues/130">#130</a
+  >. Hopefully those will be fixed upstream and this fork can be retired.
 </div>
 <div class="antialiased mx-auto my-6 max-w-3xl overflow-x-hidden px-6">
   <Header />
